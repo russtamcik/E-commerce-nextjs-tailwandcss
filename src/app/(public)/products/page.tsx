5acc655683e047fa50433157d31e94f2197c0f78
russtamcik <abbasovr455@gmail.com>
2023-10-21 06:26:41 +0500

    change

--- src/app/(public)/products/page.tsx
+++ src/app/(public)/products/page.tsx
@@ -1,74 +1,3 @@
-// "use client";
-
-// import ProductsRow from "@/components/shares/ProductsRow";
-// import request from "@/server";
-// import CategoryType from "@/types/category";
-// import ProductType from "@/types/product";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { useEffect, useState } from "react";
-
-// const Products = () => {
-//   const router = useRouter();
-//   const search = useSearchParams();
-//   const [products, setProducts] = useState([] as ProductType[]);
-//   const [total, setTotal] = useState(0);
-//   const [loading, setLoading] = useState(false);
-//   const [categories, setCategories] = useState([] as CategoryType[]);
-//   const [category, setCategory] = useState(search.get("category") || "");
-
-//   useEffect(() => {
-//     const getProducts = async () => {
-//       try {
-//         setLoading(true);
-//         const {
-//           data: { total, products },
-//         } = await request.get<{ total: number; products: ProductType[] }>(
-//           `product?page=${}&search=${search}`,
-//           { params: { category: category || undefined } }
-//         );
-//         const { data } = await request.get("category");
-//         setCategories(data);
-
-//         setProducts(products);
-//         setTotal(total);
-//       } catch (error) {
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     getProducts();
-//   }, [category, search]);
-
-//   const handleCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     const value = e.target.value;
-//     setCategory(value);
-//     if (value === "") {
-//       router.push(`products`);
-//     } else {
-//       router.push(`products?category=${value}`);
-//     }
-//   };
-
-//   return (
-//     <section style={{ marginTop: "100px" }}>
-//       <h1 className="text-center my-3">Products {total}</h1>
-//       <div>
-//         <select value={category} onChange={handleCategory}>
-//           <option value="">All</option>
-//           {categories.map((category) => (
-//             <option key={category._id} value={category._id}>
-//               {category.name}
-//             </option>
-//           ))}
-//         </select>
-//       </div>
-//       <ProductsRow loading={loading} products={products} />
-//     </section>
-//   );
-// };
-
-// export default Products;
-
 "use client";
 
 import ProductsRow from "@/components/shares/ProductsRow";
@@ -145,18 +74,10 @@ const Products = () => {
 
   return (
     <section style={{ marginTop: "100px" }}>
-      {/* <h1 className="text-center my-3">Products {total}</h1> */}
       <h1 className="py-3 my-3 border border-sky-600 text-center rounded-full text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
         Products {total}
       </h1>
       <div className="flex justify-between p-6">
-        {/* <input
-          type="text"
-          value={searchQuery}
-          onChange={handleSearch}
-          placeholder="Search..."
-        /> */}
-
         <form>
           <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
